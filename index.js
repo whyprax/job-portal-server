3d6900b4382217aa6b21537528b37a401c14a897
prathamdubey <prathamd67@gmail.com>
2024-04-30 21:06:38 +0530

    changes

--- index.js
+++ index.js
@@ -7,146 +7,109 @@ dotenv.config();
 const app = express();
 
 // Connect to MongoDB Atlas
-// Connect to MongoDB Atlas
-mongoose.connect(process.env.DB_URL);
+mongoose
+  .connect(process.env.DB_URL, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected successfully");
 
-// Define Mongoose schema for jobs
-const jobSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
-  },
-  url: {
-    type: String,
-    required: false,
-  },
-  title: {
-    type: String,
-    required: false,
-  },
-  company_name: {
-    type: String,
-    required: false,
-  },
-  company_logo: {
-    type: String,
-    required: false,
-  },
-  category: {
-    type: String,
-    required: false,
-  },
-  tags: {
-    type: [String],
-    required: false,
-  },
-  job_type: {
-    type: String,
-    required: false,
-  },
-  publication_date: {
-    type: Date,
-    required: false,
-  },
-  candidate_required_location: {
-    type: String,
-    required: false,
-  },
-  salary: {
-    type: String,
-    required: false,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-});
+    // Define Mongoose schema for jobs
+    const jobSchema = new mongoose.Schema({
+      id: {
+        type: Number,
+        required: true,
+      },
+      url: String,
+      title: String,
+      company_name: String,
+      company_logo: String,
+      category: String,
+      tags: [String],
+      job_type: String,
+      publication_date: Date,
+      candidate_required_location: String,
+      salary: String,
+      description: String,
+    });
 
-// Define Mongoose model for jobs
-const Job = mongoose.model("Job", jobSchema);
+    // Define Mongoose model for jobs
+    const Job = mongoose.model("Job", jobSchema);
 
-// Fetch remote jobs and save them to MongoDB
-async function fetchRemoteJobs() {
-  try {
-    const response = await fetch("https://remotive.com/api/remote-jobs");
-    const data = await response.json();
-    const remoteJobs = data.jobs;
-    remoteJobs.forEach(async (remoteJob) => {
+    // Fetch remote jobs and save them to MongoDB
+    async function fetchRemoteJobs() {
       try {
-        const job = new Job({
-          id: remoteJob.id,
-          url: remoteJob.url,
-          title: remoteJob.title,
-          company_name: remoteJob.company_name,
-          company_logo: remoteJob.company_logo,
-          category: remoteJob.category,
-          tags: remoteJob.tags,
-          job_type: remoteJob.job_type,
-          publication_date: new Date(remoteJob.publication_date),
-          candidate_required_location: remoteJob.candidate_required_location,
-          salary: remoteJob.salary,
-          description: remoteJob.description,
+        const response = await fetch("https://remotive.com/api/remote-jobs");
+        const data = await response.json();
+        const remoteJobs = data.jobs;
+        remoteJobs.forEach(async (remoteJob) => {
+          try {
+            const job = new Job(remoteJob);
+            await job.save();
+          } catch (error) {
+            console.error("Error saving job:", error);
+          }
         });
-        await job.save();
       } catch (error) {
-        console.error("Error saving job:", error);
+        console.error("Error fetching remote jobs:", error);
+      }
+    }
+
+    // Define route handler for /jobs/:id
+    app.get("/jobs/:id", async (req, res) => {
+      const jobId = req.params.id;
+
+      try {
+        // Query the database for the job with the specified ID
+        const job = await Job.findOne({ id: jobId });
+
+        // If job is found, send its data as response
+        if (job) {
+          res.json(job);
+        } else {
+          // If job is not found, send 404 Not Found status
+          res.status(404).json({ error: "Job not found" });
+        }
+      } catch (error) {
+        // If an error occurs, send 500 Internal Server Error status
+        res.status(500).json({ error: "Internal Server Error" });
       }
     });
-  } catch (error) {
-    console.error("Error fetching remote jobs:", error);
-  }
-}
 
-// Define route handler for /jobs/:id
-app.get("/jobs/:id", async (req, res) => {
-  const jobId = req.params.id;
+    // Define route handler for rendering HTML page
+    app.get("/", (req, res) => {
+      res.send("<h1>Server is ok at 3000</h1>");
+    });
 
-  try {
-    // Query the database for the job with the specified ID
-    const job = await Job.findOne({ id: jobId });
+    // Define route handler for /jobs
+    app.get("/jobs", async (req, res) => {
+      try {
+        // Query the database to get all jobs
+        const allJobs = await Job.find();
 
-    // If job is found, send its data as response
-    if (job) {
-      res.json(job);
-    } else {
-      // If job is not found, send 404 Not Found status
-      res.status(404).json({ error: "Job not found" });
-    }
-  } catch (error) {
-    // If an error occurs, send 500 Internal Server Error status
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+        console.log(allJobs);
+        // Send all jobs data as JSON response
+        res.json(allJobs);
+      } catch (error) {
+        // If an error occurs, send 500 Internal Server Error status
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
-// Define route handler for rendering HTML page
-app.get("/", (req, res) => {
-  res.send("<h1>Server is ok at 3000</h1>");
-});
+    // Start the server
+    const PORT = process.env.PORT || 10000;
+    const server = app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running on http://0.0.0.0:${PORT}`);
 
-// Define route handler for /jobs
-app.get("/jobs", async (req, res) => {
-  try {
-    // Query the database to get all jobs
-    const allJobs = await Job.find();
+      // Fetch remote jobs when the server starts
+      fetchRemoteJobs();
+    });
 
-    console.log(allJobs);
-    // Send all jobs data as JSON response
-    res.json(allJobs);
-  } catch (error) {
-    // If an error occurs, send 500 Internal Server Error status
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-// Start the server
-const PORT = process.env.PORT || 10000;
-const server = app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
-
-  // Fetch remote jobs when the server starts
-  fetchRemoteJobs();
-});
-
-// Increase server timeouts to prevent intermittent timeouts or connection reset errors
-server.keepAliveTimeout = 120000; // 120 seconds
-server.headersTimeout = 120000; // 120 seconds
+    // Increase server timeouts to prevent intermittent timeouts or connection reset errors
+    server.keepAliveTimeout = 120000; // 120 seconds
+    server.headersTimeout = 120000; // 120 seconds
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
